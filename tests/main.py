ca1d2dbd45a937be613ba008a66c447266a4f8ef
Henrisen <56893232+Henrisen@users.noreply.github.com>
2023-12-18 15:58:59 +0100

    Delete tests directory

--- tests/main.py
+++
@@ -1,6 +0,0 @@
-def test_hash1():
-  import almostUHM
-  demo = almostUHM.generatePrecidualHashFunction(1234)
-  hash = demo('Test String!')
-  return '35725045e467693a5d24f86952dd881508824009ad2be8b1070c567aa6d448fc' == hash
-  
